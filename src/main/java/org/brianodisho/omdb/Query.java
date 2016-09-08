7b71bdf6a1234af5d243cdae7a28a1721d15d0ab
Brian Odisho <brianodisho@gmail.com>
2016-09-07 20:19:13 -0400

    Updated to use builder pattern for constructing Query objects.

--- src/main/java/org/brianodisho/omdb/Query.java
+++ src/main/java/org/brianodisho/omdb/Query.java
@@ -1,54 +1,76 @@
 package org.brianodisho.omdb;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
- * Created by brianodisho on 8/22/16.
+ * Created by brianodisho on 8/31/16.
  */
 public class Query {
 
-    private final Map<String, String> queryMap = new HashMap<>(5);
+    private final Map<String, String> queryMap = new HashMap<>(6);
 
-    public boolean containsKey(Object key) {
-        return queryMap.containsKey(key);
+
+    private Query(Builder builder) {
+        queryMap.put("t", builder.title);
+        queryMap.put("i", builder.imdbID);
+        queryMap.put("type", builder.type);
+        queryMap.put("y", builder.year);
+        queryMap.put("page", builder.page);
     }
 
-    public String get(Object key) {
-        return queryMap.get(key);
-    }
-
-    public String put(String key, String value) {
-        switch (key) {
-            case "i":
-            case "s":
-            case "t":
-            case "y":
-                queryMap.put(key, value);
-                return value;
-            case "type":
-                if (value.equals("movie") || value.equals("series") || value.equals("episode")) {
-                    queryMap.put(key, value);
-                    return value;
-                } else {
-                    return null;
-                }
-            case "plot":
-                if (value.equals("short") || value.equals("long")) {
-                    queryMap.put(key, value);
-                    return value;
-                } else {
-                    return null;
-                }
-            default:
-                return null;
-        }
-    }
 
     public Map<String, String> getQueryMap() {
         return queryMap;
+    }
+
+
+
+    public static class Builder {
+
+        private String title = ""; // Required Param
+        private String imdbID = ""; // Optional Param
+        private String type = ""; // Optional Param
+        private String year = ""; // Optional Param
+        private String page = ""; // Optional Param
+
+        public Builder(String query) {
+            if (query.startsWith("tt")) {
+                this.imdbID = query;
+            } else {
+                this.title = query;
+            }
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder imdbID(String imdbID) {
+            this.imdbID = imdbID;
+            return this;
+        }
+
+        public Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder year(String year) {
+            this.year = year;
+            return this;
+        }
+
+        public Builder page(String page) {
+            this.page = page;
+            return this;
+        }
+
+        public Query build() {
+            return new Query(this);
+        }
+
     }
 
 }
