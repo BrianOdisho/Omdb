8d01abd73eed731a9be21bd2099d8586de6a7094
Brian Odisho <brianodisho@gmail.com>
2016-09-07 20:19:54 -0400

    Update to extend Exception.

--- src/main/java/org/brianodisho/omdb/OmdbException.java
+++ src/main/java/org/brianodisho/omdb/OmdbException.java
@@ -1,26 +1,34 @@
 package org.brianodisho.omdb;
 
+import java.io.IOException;
+
 /**
- * Created by brianodisho on 8/26/16.
+ * Created by brianodisho on 9/7/16.
  */
 public class OmdbException extends Exception {
 
     public OmdbException() {
+        super();
     }
+
 
     public OmdbException(String message) {
         super(message);
     }
 
+
     public OmdbException(String message, Throwable cause) {
         super(message, cause);
     }
+
 
     public OmdbException(Throwable cause) {
         super(cause);
     }
 
-    public OmdbException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+
+    protected OmdbException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
+
 }
